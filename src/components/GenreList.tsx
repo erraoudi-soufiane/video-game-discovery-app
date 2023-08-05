15d77a7573bc1b553798bb29c57151889b4bd15d
erraoudi-soufiane <erraoudisoufiane5@gmail.com>
2023-08-05 16:31:00 +0100

    creating a generic data fetching hook

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -2,13 +2,16 @@ import { Text, Stack } from "@chakra-ui/react";
 import useGenres from "../hooks/useGenres";
 
 const GenreList = () => {
-  const { genres } = useGenres();
+  const { data, error, isLoading } = useGenres();
   return (
-    <Stack justifyContent={"space-between"} padding="20px">
-      {genres.map((genre) => (
-        <Text key={genre.id}> {genre.name} </Text>
-      ))}
-    </Stack>
+    <>
+      {error && <Text> {error} </Text>}
+      <Stack justifyContent={"space-between"} padding="20px">
+        {data.map((genre) => (
+          <Text key={genre.id}> {genre.name} </Text>
+        ))}
+      </Stack>
+    </>
   );
 };
 
--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,28 +1,12 @@
-import { useEffect, useState } from "react";
-import apiClient from "../services/api-client";
+import useData from "./useData";
 
 interface Genres {
   id: number;
   name: string;
 }
 
-interface FetchGenresResponse {
-  count: number;
-  results: Genres[];
-}
-
 const useGenres = () => {
-  const [genres, setGenres] = useState<Genres[]>([]);
-  const [error, setError] = useState("");
-  const [isLoading, setLoading] = useState(false);
-
-  useEffect(() => {
-    apiClient
-      .get<FetchGenresResponse>("/genres")
-      .then((res) => setGenres(res.data.results));
-  });
-
-  return { genres };
+  return useData<Genres>("/genres");
 };
 
 export default useGenres;

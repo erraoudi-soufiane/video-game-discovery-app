98cc949a744d47cb8b673a75f12f7991a3435214
erraoudi-soufiane <erraoudisoufiane5@gmail.com>
2023-08-05 14:26:03 +0100

    correcting some layout issues

--- src/components/GameCardSkeleton.tsx
+++ src/components/GameCardSkeleton.tsx
@@ -2,7 +2,7 @@ import { Card, CardBody, Skeleton, SkeletonText } from "@chakra-ui/react";
 
 const GameCardSkeleton = () => {
   return (
-    <Card width={"260px"} borderRadius={10} overflow="hidden">
+    <Card width={"310px"} borderRadius={10} overflow="hidden">
       <Skeleton height={"200px"}></Skeleton>
       <CardBody>
         <SkeletonText />
